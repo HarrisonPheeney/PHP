1b9b82e2b206a4ccc5410801cbd380c18942a890
HarrisonPheeney <harrison.pheeney@hotmail.ca>
2015-09-28 20:04:47 -0300

    first version of temp output

---
+++ Lab_2/FahrenheitConversion.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: inet2005
+ * Date: 9/25/15
+ * Time: 12:52 PM
+ */
+
+
+for ($x = 0; $x < 100; $x++) {
+
+    $Celsius = Math.round(($x - 32) * .56);
+
+    echo $x." degree(s) fahrenheit equals ".$Celsius." degree(s) Celsius"."<br \>";;
+
+}
+
+
+?>
